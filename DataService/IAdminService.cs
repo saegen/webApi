bb8fe412aa587aca0dfa32fbe31b3198e7c3b25d
Tomas Hesse <tomas.hesse@consid.se>
2017-10-03 13:32:32 +0200

    Lagt till klass och interface för Admin

---
+++ DataService/IAdminService.cs
@@ -0,0 +1,36 @@
+﻿using DataService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Common;
+
+namespace DataService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAdminService" in both code and config file together.
+    [ServiceContract]
+    public class AdminService : IAdminService
+    {
+        public void Subscribe(int userId, IEnumerable<ApiSubscription> subscriptions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe(int userId, Guid subscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe(int userId, IEnumerable<ApiSubscription> subscriptions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnsubscribeAll(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataService/Interfaces/IAdminService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using System.ServiceModel;
+
+namespace DataService.Interfaces
+{
+    [ServiceContract(Namespace = "http://dataservice/interfaces/admin")]
+    public interface IAdminService
+    {
+        [OperationContract]
+        void Subscribe(int userId, IEnumerable<ApiSubscription> subscriptions);
+
+        [OperationContract]
+        void Unsubscribe(int userId, Guid subscriptionId);
+
+        [OperationContract]
+        void Unsubscribe(int userId, IEnumerable<ApiSubscription> subscriptions);
+
+        [OperationContract]
+        void UnsubscribeAll(int userId);
+
+    }
+}
