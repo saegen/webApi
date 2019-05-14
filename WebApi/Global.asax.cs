6a66fc71cad89354750f47f239d133a66979702d
saegen <saegen@hotmail.com>
2019-05-14 10:06:30 +0200

    NLog introducerat. Skulle med tidigare. NLog reggas i Application_Start

--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,7 @@ namespace WebApi
     {
         protected void Application_Start()
         {
+            LogManager.LoadConfiguration("nlog.config");
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
