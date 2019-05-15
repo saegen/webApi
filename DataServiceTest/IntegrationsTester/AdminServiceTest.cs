9dc280619f10b85c7674a0c74da03647728e25c6
saegen <saegen@hotmail.com>
2019-05-15 15:34:39 +0200

    Lagt till testfall för Unsubscribe. Väldigt messy kod, TestSubscribe är nästlat, men testet är bra och det gick igenom.

--- DataServiceTest/IntegrationsTester/AdminServiceTest.cs
+++ DataServiceTest/IntegrationsTester/AdminServiceTest.cs
@@ -19,32 +19,56 @@ namespace DataServiceTest.IntegrationsTester
         private int _userId = -1, _numSubs = -1;
         private Guid _subId = Guid.Empty;
         private List<Common.ApiSubscription> subs = new List<Common.ApiSubscription>();
-        private IEnumerable<Guid> subIDs = null;
+        private IEnumerable<Guid> existingSubIDs = null;
         private CreateUserDTO testUser = new CreateUserDTO() { Email = "admin@service.se", FirstName = "TestUser", LastName = "AdminServiceTest" };
 
+        /// <summary>
+        /// Skapar en testanvändare och hämtar upp alla Subscriptions. 
+        /// När testanvändaren tas bort i Cleanup så försvinner eventuella prenumerationer.
+        /// </summary>
         [TestInitialize]
         public void TestInit()
         {
             _SubClient = new SubscriptionServiceClient();
-            subIDs = _SubClient.GetSubscriptions().Select(item => item.Id).ToArray();
-            _numSubs = subIDs.Count();
+            existingSubIDs = _SubClient.GetSubscriptions().Select(item => item.Id).ToArray();
+            _numSubs = existingSubIDs.Count();
             _SubClient.CloseOrAbortService();
             _UserClient = new UserServiceClient();
             _userId = _UserClient.CreateUser(testUser).Id;
             _UserClient.CloseOrAbortService();
+            Assert.IsTrue(_userId > 0, "TestInit: Kunde inte skapa testuser! Inga unittest kommer att fungera");
+            Assert.IsTrue(_numSubs > 0, "TestInit: Finns inga subscriptions att testa med! Inga unittest kommer att fungera");
         }
+        /// <summary>
+        /// Testanvändaren prenumererar på alla som finns. (Snabbhack)
+        /// </summary>
         [TestMethod]
-        public void TestAdminService()
+        public void TestSubscribe()
         {
-            Assert.IsTrue(_userId > 0,"Kunde inte skapa testuser!");
-            Assert.IsTrue(_numSubs > 0, "Finns inga subscriptions att testa med!");
             _AdminClient = new AdminServiceClient();
-            _AdminClient.Subscribe(_userId,(Guid[])subIDs);
+            _AdminClient.Subscribe(_userId,(Guid[])existingSubIDs);
             int userSubNum = _AdminClient.GetUserSubscriptions(_userId).Count();
             _AdminClient.CloseOrAbortService();
             Assert.IsTrue(userSubNum == _numSubs, "userId=" + _userId + ", userSubNum =" + userSubNum + ", _numSubs =" + _numSubs);
         }
 
+        [TestMethod]
+        public void TestUnsubscribe()
+        {
+            this.TestSubscribe(); //lite fulhack men ...
+            var unsubscribeId = existingSubIDs.First();
+            _AdminClient = new AdminServiceClient();
+            _AdminClient.Unsubscribe(_userId, unsubscribeId);
+            var userSubNum = _AdminClient.GetUserSubscriptions(_userId);
+            _AdminClient.CloseOrAbortService();
+            var nonExisting = userSubNum.Select(sub => sub.Id).ToList();
+            Assert.IsFalse(nonExisting.Contains(unsubscribeId),"Testaren har prenumerationen kvar");
+            Assert.IsTrue(userSubNum.Count() == _numSubs - 1, "Testaren borde ha alla prenumerationen -minus den vi tog bort");
+
+        }
+        /// <summary>
+        /// Tar bort testanvändaren och hens prenumerationer
+        /// </summary>
         [TestCleanup]
         public void CleanUp()
         {
