4072e5e14d31e51febbb6a67c1efcb9e8da3eafa
saegen <saegen@hotmail.com>
2019-06-03 15:05:39 +0200

    För att komma åt localhost, sqlexpress från Containers så måste man. 1 konfigurera SqlServer att Enabla TCP/IP. 2. Skapa en SQL Login. 3. Skriv connectionstring enligt ovan, se web.config. Lade till en koll att man kommer åt databasen i WCF global.asax.session_start. Det var för att det verkar som att host.docker.internal i hosts ändras ibland.  host.docker.internal går dock inte att använda i constring. Jättemärkligt men sant.

--- DataService/Global.asax.cs
+++ DataService/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using NLog;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Configuration;
 
 namespace DataService
 {
@@ -26,7 +28,7 @@ namespace DataService
 
         protected void Session_Start(object sender, EventArgs e)
         {
-
+            TestDbConnection();
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
@@ -52,6 +54,25 @@ namespace DataService
         protected void Application_End(object sender, EventArgs e)
         {
 
+        }
+
+        private void TestDbConnection()
+        {
+            string shorty = ConfigurationManager.ConnectionStrings["short"].ToString();
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(shorty);
+                con.Open();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                //log.Error("Can't open connection to database: {0}", dockerConIP);
+
+                throw new Exception("Can't open/access database");
+                //throw new FaultException("Can't open connection to database");
+            }
         }
     }
 }
