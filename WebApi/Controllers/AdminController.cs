2ad7b735f676dbe70c2a4d77c50071cdd6da319e
saegen <saegen@hotmail.com>
2019-07-18 07:14:25 +0200

    #52 AdminController har alla metoder implementerade men är fejk async och admin repo är inte fixad än

--- WebApi/Controllers/AdminController.cs
+++ WebApi/Controllers/AdminController.cs
@@ -1,44 +1,31 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using NLog;
 using WebApi.Models;
-using WebApi.SubscriptionService;
-using WebApi.UserService;
 using WebApi.AdminService;
 namespace WebApi.Controllers
 {
     public class AdminController : ApiController
     {
         private Logger log;
-        private ISubscriptionRepo _subRepo;
-        private IUserRepo _userRepo;
         private IAdminRepo _adminRepo;
         public AdminController()
         {
             log = LogManager.GetCurrentClassLogger();
-            UserServiceClient userClient = new UserServiceClient();
-            _userRepo = new UserRepo(userClient);
-            SubscriptionServiceClient subClient = new SubscriptionServiceClient();
-            _subRepo = new SubscriptionRepo(subClient);
             AdminServiceClient _adminClient = new AdminServiceClient();
             _adminRepo = new AdminRepo(_adminClient);
         }
 
-  
         [Route("Admin/Subscriptions/{userId}")]
         [HttpGet]
         public async Task<IEnumerable<ApiSubscription>> GetUserSubscriptionsAsync(int userId)
         {
             return await Task.Run(() => _adminRepo.GetUserSubscriptions(userId));
         }
-        
-        //IEnumerable<ApiUser> GetSubscriptionUsers(Guid subscriptionId);
+
         [Route("Admin/Users/{subscriptionId}")]
         [HttpGet]
         public async Task<IEnumerable<ApiUser>> GetSubscriptionUsersAsync(Guid subscriptionId)
@@ -50,18 +37,22 @@ namespace WebApi.Controllers
         [HttpDelete]
         public async Task UnsubscribeAsync(int userId, Guid subscriptionId)
         {
-             await Task.Run(() => _adminRepo.Unsubscribe(userId,subscriptionId));
+            await Task.Run(() => _adminRepo.Unsubscribe(userId, subscriptionId));
         }
-        //void Unsubscribe(int userId, Guid subscriptionId);
-        //void Subscribe(int userId, Guid[] subscriptionIds);
-        //void UpdateUserSubscription(int userId, ApiSubscription subscriptionData);
 
-        //[Route("Admin")]
+        [Route("Admin")]
+        [HttpPost]
+        public async Task SubscribeAsync(int userId, Guid[] subscriptionIds)
+        {
+            await Task.Run(() => _adminRepo.Subscribe(userId, subscriptionIds));
+        }
 
-        //public IEnumerable<ApiUser> GetUsers()
-        //{
-        //    log.Debug("WebApi GetUsers()");
-        //    return repo.GetUsers();
-        //}
+        [Route("Admin")]
+        [HttpPut]
+        public async Task UpdateUserSubscriptionAsync(int userId, ApiSubscription subscriptionData)
+        {
+            await Task.Run(() => _adminRepo.UpdateUserSubscription(userId, subscriptionData));
+        }
+
     }
 }
