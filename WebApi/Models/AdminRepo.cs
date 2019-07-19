06f01cc080c506c8122b5d3224a79bf7f9b053db
saegen <saegen@hotmail.com>
2019-07-19 09:24:58 +0200

    #53, Alla metoder är implementerade.

--- WebApi/Models/AdminRepo.cs
+++ WebApi/Models/AdminRepo.cs
@@ -5,11 +5,13 @@ using System.Web;
 using WebApi.AdminService;
 using Common;
 using System.ServiceModel;
+using NLog;
 
 namespace WebApi.Models
 {
     public class AdminRepo : IAdminRepo
     {
+        private Logger log = LogManager.GetCurrentClassLogger();
         private AdminServiceClient _adminClient;
 
         public AdminRepo(AdminServiceClient adminClient)
@@ -26,25 +28,59 @@ namespace WebApi.Models
 
         public void Subscribe(int userId, Guid[] subscriptionIds)
         {
-            foreach (var sub in subscriptionIds)
-            {
-                _adminClient.Subscribe(userId, subscriptionIds);
-            }
+                try
+                {
+                    _adminClient.Subscribe(userId, subscriptionIds);
+                }
+                catch (Exception ex)
+                {
+                    log.Error("AdminRepo: Subscribe: " + userId + ": " + ex.Message);
+                    throw;
+                }            
         }
 
         public IEnumerable<ApiUser> GetSubscriptionUsers(Guid subscriptionId)
         {
-          return  _adminClient.GetSubscriptionUsers(subscriptionId);
+            ApiUser[] users;
+            try
+            {
+                users = _adminClient.GetSubscriptionUsers(subscriptionId);
+            }
+            catch (Exception ex) 
+            {
+                log.Error("AdminRepo: GetSubscriptionUsers: " + subscriptionId + ": " + ex.Message);
+                throw;
+            }
+            return users;
         }
 
         public IEnumerable<ApiSubscription> GetUserSubscriptions(int userId)
         {
-            return _adminClient.GetUserSubscriptions(userId);
+            ApiSubscription[] subs;
+            try
+            {
+                subs = _adminClient.GetUserSubscriptions(userId);
+            }
+            catch (Exception ex)
+            {
+                log.Error("AdminRepo: GetUserSubscriptions: " + userId + ": " + ex.Message);
+                throw;
+            }
+            return subs;
         }
 
         public void Unsubscribe(int userId, Guid subscriptionId)
         {
-            _adminClient.Unsubscribe( userId,  subscriptionId);
+            try
+            {
+                _adminClient.Unsubscribe(userId, subscriptionId);
+            }
+            catch (Exception ex)
+            {
+                log.Error("AdminRepo: Unsubscribe: " + userId + ", " + subscriptionId + " : " + ex.Message);
+                throw;
+            }
+            
         }
 
     }
