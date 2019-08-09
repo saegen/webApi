16b9aa7b9a6288cd601f4dd50e67a820b46a5c7b
saegen <saegen@hotmail.com>
2019-08-09 07:08:35 +0200

    #69 Första js scriptet.

---
+++ AngularFront/MockServer/js/userjs.js
@@ -0,0 +1,11 @@
+$('.lsUser').on('click', function() {
+    alert('CLKDJISDJ');
+    /* var $this = $(this);
+    var $alias = $this.data('alias');
+
+    $('.active').removeClass('active');
+    $this.toggleClass('active')
+
+    // Pass clicked link element to another function
+    myfunction($this, $alias) */
+})
